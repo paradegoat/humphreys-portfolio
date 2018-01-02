cdeddb2cde1591e88d55de0ac4381ac468f92b64
Geoff Humphreys <humphreys.geoff@gmail.com>
2018-01-02 15:47:03 -0500

    Implemented custom scoops for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,8 @@
 class Portfolio < ApplicationRecord
   validates_presence_of :title, :body, :main_image, :thumb_image
+  def self.angular
+    where(subtitle: "angular")
+  end
 
+  scope :ruby_on_rails, -> {where(subtitle: "Ruby on Rails")}
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
+3.times do |topic|
+  Topic.create!(
+    title: "Topic #{topic}"
+  )
+end
+puts "3 Topics created"
+
 10.times do |blog|
   Blog.create!(
     title: "my blog post #{blog}",
-    body: 'butt butt butt butt butt butt butt butt butt butt butt butt butt butt butt butt butt butt butt butt butt butt butt butt butt butt butt butt butt butt butt butt butt butt butt butt'
+    body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur mattis cursus dignissim. Suspendisse lacus erat, dignissim eu blandit non, dictum a tortor. Vestibulum accumsan dapibus libero faucibus faucibus. Pellentesque rhoncus pellentesque nunc at egestas. Morbi ultrices sapien quis ligula venenatis sagittis. Proin faucibus orci vel risus ullamcorper ultrices. Morbi viverra ornare mauris, eu hendrerit justo viverra dignissim. Morbi nec blandit enim, ut laoreet nunc. Pellentesque metus eros, feugiat vel purus at, egestas sollicitudin quam. Maecenas non vestibulum massa. Phasellus dictum massa pharetra, placerat dui gravida, efficitur mauris. Morbi vitae laoreet nisl, sed bibendum massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Mauris a risus ligula. Morbi egestas mi sed nisi mollis semper. In hac habitasse platea dictumst.',
+    topic_id: Topic.last.id
   )
 end
 
@@ -16,11 +24,21 @@ end
 
 puts "5 skills created"
 
-9.times do |portfolio_item|
+8.times do |portfolio_item|
   Portfolio.create!(
     title: "portfolio title #{portfolio_item}",
-    subtitle: 'my great service',
-    body: 'butt butt butt butt butt butt butt butt butt butt butt butt butt butt butt butt butt butt butt butt butt butt butt butt butt butt butt butt butt butt butt butt butt butt butt butt',
+    subtitle: 'Ruby on Rails',
+    body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur mattis cursus dignissim. Suspendisse lacus erat, dignissim eu blandit non, dictum a tortor. Vestibulum accumsan dapibus libero faucibus faucibus. Pellentesque rhoncus pellentesque nunc at egestas. Morbi ultrices sapien quis ligula venenatis sagittis. Proin faucibus orci vel risus ullamcorper ultrices. Morbi viverra ornare mauris, eu hendrerit justo viverra dignissim. Morbi nec blandit enim, ut laoreet nunc. Pellentesque metus eros, feugiat vel purus at, egestas sollicitudin quam. Maecenas non vestibulum massa. Phasellus dictum massa pharetra, placerat dui gravida, efficitur mauris. Morbi vitae laoreet nisl, sed bibendum massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Mauris a risus ligula. Morbi egestas mi sed nisi mollis semper. In hac habitasse platea dictumst.',
+    main_image: "http://via.placeholder.com/600x4 00",
+    thumb_image: "http://via.placeholder.com/350x200"
+  )
+end
+
+1.times do |portfolio_item|
+  Portfolio.create!(
+    title: "portfolio title #{portfolio_item}",
+    subtitle: 'angular',
+    body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur mattis cursus dignissim. Suspendisse lacus erat, dignissim eu blandit non, dictum a tortor. Vestibulum accumsan dapibus libero faucibus faucibus. Pellentesque rhoncus pellentesque nunc at egestas. Morbi ultrices sapien quis ligula venenatis sagittis. Proin faucibus orci vel risus ullamcorper ultrices. Morbi viverra ornare mauris, eu hendrerit justo viverra dignissim. Morbi nec blandit enim, ut laoreet nunc. Pellentesque metus eros, feugiat vel purus at, egestas sollicitudin quam. Maecenas non vestibulum massa. Phasellus dictum massa pharetra, placerat dui gravida, efficitur mauris. Morbi vitae laoreet nisl, sed bibendum massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Mauris a risus ligula. Morbi egestas mi sed nisi mollis semper. In hac habitasse platea dictumst.',
     main_image: "http://via.placeholder.com/600x4 00",
     thumb_image: "http://via.placeholder.com/350x200"
   )

a95abf579b9476b5649bd515b12cdd600c0c481e
Geoff Humphreys <humphreys.geoff@gmail.com>
2018-02-12 13:16:53 -0500

    Updated styles for master layout file

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@
   get 'angular-items', to: 'portfolios#angular'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
-  get 'about', to: 'pages#about'
+  get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
 
   resources :blogs do

d92683537c118e085965fe5165036e62c3439393
Geoff Humphreys <humphreys.geoff@gmail.com>
2018-02-15 17:10:50 -0500

    Created a comment broadcast, configured websocket route, updated comment model with a callback

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@
     end
   end
 
+  mount ActionCable.server => '/cable'
+
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

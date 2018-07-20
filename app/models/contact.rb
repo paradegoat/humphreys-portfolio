9ad626a4349418afc364034be0948a538d7e4ba8
Geoff Humphreys <humphreys.geoff@gmail.com>
2018-07-20 17:17:32 -0400

    Deleted duplicate code

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -12,20 +12,3 @@ class Contact < MailForm::Base
     }
   end
 end
-
-
-
-class Contact < MailForm::Base
-  attribute :name, :validate => true
-  attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
-  attribute :message,   :validate => true
-  attribute :nickname,  :captcha  => true
-
-  def headers
-    {
-      :subject => "K-9 Capsules has visitor",
-      :to => ENV["ADMIN_EMAIL"],
-      :from => %("#{name}" <#{email}>)
-    }
-  end
-end
